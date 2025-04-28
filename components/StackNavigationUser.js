8769b6d855866aba91ebc5f589d0dad88a4276c7
Rahul Bhati <rahulbhati0202@gmail.com>
2025-04-28 20:33:39 +0530

    seprate the file for navigation

--- components/StackNavigationUser.js
+++ components/StackNavigationUser.js
@@ -1,20 +1,34 @@
 import React from 'react'
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
 import HomeStack from './HomeStack';
-import About from "./HomeStack"
-
-
+import { About } from './HomeStack';
+import { Button, Text, View } from 'react-native'
 
 const Stack = createStackNavigator();
-const StackNavigationUser = () => {
 
+const StackNavigationUser = () => {
     return (
         <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name='Home' component={HomeStack} />
+            <Stack.Navigator
+                screenOptions={{
+                    headerStyle: { backgroundColor: "red", },
+                    headerTitleStyle: { fontSize: 30, },
+                    headerTintColor: { color: "white" }
+                }}>
+                <Stack.Screen
+                    options={{
+                        headerLeft: () => (
+                            <Button title='Left' onPress={() => alert('Left button pressed')} />
+                        ),
+                        headerRight: () => (
+                            <Button title='Right' onPress={() => alert('Right button pressed')} />
+                        ),
+                        headerTitle: 'Your Screen Title',
+                        headerTintColor: "white"
+                    }}
+                    name='Home' component={HomeStack} />
                 <Stack.Screen name='About' component={About} />
-
             </Stack.Navigator>
         </NavigationContainer>
     )
