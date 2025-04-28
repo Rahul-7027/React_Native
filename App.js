4f78501352442f4c5a0f7ddd57e4f05b71dbbebc
Rahul Bhati <rahulbhati0202@gmail.com>
2025-04-28 19:42:31 +0530

    Stack Navigation my command is heree npm install @react-navigation/native @react-navigation/stack react-native-screens react-native-safe-area-context react-native-gesture-handler react-native-reanimated

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View } from 'react-native'
+import { Button, Text, View } from 'react-native'
 import ResponsiveLayout from './components/ResponsiveLayout'
 import ButtonColor from './components/ButtonColor'
 import RadioButton from './components/RadioButton'
@@ -10,12 +10,36 @@ import PressableEvent from './components/PressableEvent'
 import Status from './components/Status'
 import PlatFormDevice from './components/PlatFormDevice'
 import CustomModal from './components/CustomModal'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+
+const Stack = createStackNavigator();
 
 const App = () => {
   return (
-   <View>
-    <CustomModal/>
-   </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Home' component={Home} />
+        <Stack.Screen name='About' component={About} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+const Home=({navigation})=>{
+  return(
+    <View>
+      <Text>Home Screen</Text>
+      <Button title='Go to about Page' onPress={()=>{navigation.navigate("About")}}/>
+    </View>
+  )
+}
+
+const About=()=>{
+  return(
+    <View>
+      <Text>About Screen</Text>
+    </View>
   )
 }
 
